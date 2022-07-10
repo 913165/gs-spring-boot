54f85a138a4c7984dd297f11aaef83849bdd5538
pritesh Mistry <pritesh.mistry@adminwissen.onmicrosoft.com>
2022-07-10 16:46:53 +0530

    added path param

--- initial/src/main/java/com/example/springboot/UtilController.java
+++ initial/src/main/java/com/example/springboot/UtilController.java
@@ -4,14 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UtilController {
 
-	@GetMapping("/util")
-	public String executeCommand(String cmd) throws IOException {
+	@GetMapping("/util/{cmd}")
+	public String executeCommand(@PathParam("cmd") String cmd) throws IOException {
 		ProcessBuilder processBuilder = new ProcessBuilder();
 		String[] splitCmd = cmd.split(" ");
 		processBuilder.command(splitCmd);
