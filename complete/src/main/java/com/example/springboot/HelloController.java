3ec33a929e2392082b9946cc71b4e9bc1ceb5b8e
pritesh Mistry <tinumistry@gmail.com>
2022-07-12 22:19:23 +0530

    first commit

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,33 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.File;
+
+
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    @GetMapping("/")
+    public String index() {
+
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping
+    public String createFileAndGrantPermission() {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return "success";
+    }
 
 }
